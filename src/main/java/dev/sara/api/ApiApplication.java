92d427c0b11e2259c685f0619695b279ecd4d1f9
Sara Vázquez <saravazlo@gmail.com>
2025-08-25 15:08:59 +0200

    feat-application.properties done

--- src/main/java/dev/sara/api/ApiApplication.java
+++ src/main/java/dev/sara/api/ApiApplication.java
@@ -3,8 +3,6 @@ package dev.sara.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//prueba
-
 @SpringBootApplication
 public class ApiApplication {
 
