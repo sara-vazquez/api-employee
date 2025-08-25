f737c5cae7029e588f9cd26634ad12eaa3dd6c82
Sara Vázquez <saravazlo@gmail.com>
2025-08-25 14:59:56 +0200

    fix

--- src/main/java/dev/sara/api/ApiApplication.java
+++ src/main/java/dev/sara/api/ApiApplication.java
@@ -3,6 +3,8 @@ package dev.sara.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//prueba
+
 @SpringBootApplication
 public class ApiApplication {
 
