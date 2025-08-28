6bafca8bf330f62b128253366da93be7dd2c931a
Sara Vázquez <saravazlo@gmail.com>
2025-08-28 08:55:32 +0200

    fix-added id to the entity

--- src/main/java/dev/sara/request/RequestEntity.java
+++ src/main/java/dev/sara/request/RequestEntity.java
@@ -2,21 +2,36 @@ package dev.sara.request;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class RequestEntity {
     
+    @Id //define el campo 'id' como primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //genera el id automáticamente
+    private Long id;
     private String name;
     private LocalDate date;
     private String topic;
     private String description;
 
-    public RequestEntity(String name, LocalDate date, String topic, String description ) {
+    public RequestEntity(Long id, String name, LocalDate date, String topic, String description ) {
+        this.id = id;
         this.name = name;
         this.date = date;
         this.topic = topic;
         this.description = description;
-        
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
     public String getName() {
         return name;
     }
