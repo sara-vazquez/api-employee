90ac602cdba24f7914bdfba85f4180d3296ed670
Sara Vázquez <saravazlo@gmail.com>
2025-08-29 14:23:18 +0200

    feat-updated Service and Controller

--- src/main/java/dev/sara/request/RequestServiceImpl.java
+++ src/main/java/dev/sara/request/RequestServiceImpl.java
@@ -1,12 +1,13 @@
 package dev.sara.request;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 import dev.sara.implementations.IGenericService;
 
 @Service
-public class RequestServiceImpl implements IGenericService<RequestEntity> {
+public class RequestServiceImpl implements IGenericService<RequestDTOResponse, RequestDTORequest> {
 
     private final RequestRepository repository;
 
@@ -15,7 +16,21 @@ public class RequestServiceImpl implements IGenericService<RequestEntity> {
     }
 
     @Override
-    public List<RequestEntity> getEntities() {
-        return repository.findAll();
+    public List<RequestDTOResponse> getEntities() {
+        List<RequestDTOResponse> requests = new ArrayList<>();
+
+        repository.findAll().forEach(r -> {
+            RequestDTOResponse request = RequestMapper.toDTO(r);
+            requests.add(request);
+        });
+
+        return requests;
+    }
+
+    @Override
+    public RequestDTOResponse storeEntity(RequestDTORequest requestDTORequest) {
+        RequestEntity request = RequestMapper.toEntity(requestDTORequest);
+        RequestEntity requestStored = repository.save(request);
+        return RequestMapper.toDTO(requestStored);
     }
 }
