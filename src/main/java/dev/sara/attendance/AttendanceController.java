acb327c9f861cd9add3e06d27bd7dc1797dc301c
Sara Vázquez <saravazlo@gmail.com>
2025-09-04 19:51:41 +0200

    feat-attendance controller done

---
+++ src/main/java/dev/sara/attendance/AttendanceController.java
@@ -0,0 +1,36 @@
+package dev.sara.attendance;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.sara.implementations.IAttendanceService;
+
+@RestController
+@RequestMapping(path = "${api-endpoint}/attendances")
+public class AttendanceController {
+
+    private final IAttendanceService service;
+
+    public AttendanceController(IAttendanceService service) {
+        this.service = service;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<AttendanceDTOResponse> markAsAttended(@RequestBody AttendanceDTORequest dtoRequest) {
+        if (dtoRequest.attendedAt() == null || dtoRequest.request() == null || dtoRequest.technician() == null) {
+        return ResponseEntity.badRequest().build();
+        }
+
+        AttendanceDTOResponse response = service.markAsAttended(dtoRequest);
+
+        if (response == null) {
+        return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.status(201).body(response);
+    }
+    
+   
+}
