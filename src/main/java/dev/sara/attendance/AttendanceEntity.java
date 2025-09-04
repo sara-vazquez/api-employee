7709753935ddd583549b25ff6870489cfcad1c7b
Sara Vázquez <saravazlo@gmail.com>
2025-09-04 14:24:40 +0200

    fix-updated attendanceentity with columns

--- src/main/java/dev/sara/attendance/AttendanceEntity.java
+++ src/main/java/dev/sara/attendance/AttendanceEntity.java
@@ -2,11 +2,15 @@ package dev.sara.attendance;
 
 import java.time.LocalDateTime;
 
+import dev.sara.request.RequestEntity;
+import dev.sara.technician.TechnicianEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 
@@ -19,12 +23,24 @@ public class AttendanceEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "time", nullable= false)
+    @Column(name = "attended_at", nullable= false)
     private LocalDateTime attendedAt;
 
-    public AttendanceEntity(LocalDateTime attendedAt) {
+    @OneToOne
+    @JoinColumn(name = "request_id", nullable = false, unique = true)
+    private RequestEntity request;
+
+    @OneToOne
+    @JoinColumn(name = "technician_id", nullable = false, unique = true)
+    private TechnicianEntity technician;
+
+    public AttendanceEntity(LocalDateTime attendedAt, RequestEntity request, TechnicianEntity technician) {
+        this.attendedAt = attendedAt;
+        this.request = request;
+        this.technician = technician;
 
     }
+
     public AttendanceEntity() {}
 
     public Long getId() {
@@ -41,6 +57,22 @@ public class AttendanceEntity {
 
     public void setAttendedAt(LocalDateTime attendedAt) {
         this.attendedAt = attendedAt;
+    }
+
+    public RequestEntity getRequest() {
+        return request;
+    }
+
+    public void setRequest(RequestEntity request) {
+        this.request = request;
+    }
+
+    public TechnicianEntity getTechnician() {
+        return technician;
+    }
+
+    public void setTechnician(TechnicianEntity technician) {
+        this.technician = technician;
     }
 
 }
